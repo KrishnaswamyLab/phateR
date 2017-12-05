7e04663b279c4635519efd603010d79e65abb1d1
Robert M Flight <r.flight79@gmail.com>
2017-12-05 13:51:33 -0500

    document the data

---
+++ R/tree.data.R
@@ -0,0 +1,8 @@
+#' Fake branching data for examples
+#'
+#' A dataset containing high dimensional data that has branchpoints
+#'
+#' @format A data.frame with 1440 rows and 61 variables
+#'
+#' @source The authors
+"tree.data"
