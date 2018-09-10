b9869328546c00df8fa21c741ddc797a4e80be5a
Scott Gigante <scottgigante@gmail.com>
2018-09-10 19:26:53 -0400

    manually import utils::menu

--- R/utils.R
+++ R/utils.R
@@ -18,7 +18,7 @@ load_pyphate <- function(delay_load = FALSE) {
   if (methods::is(result, "try-error")) {
     if (length(grep("ModuleNotFoundError: No module named 'phate'", result)) > 0 ||
         length(grep("ImportError: No module named phate", result)) > 0) {
-      if (menu(c("Yes", "No"), title="Install PHATE Python package with reticulate?") == 1) {
+      if (utils::menu(c("Yes", "No"), title="Install PHATE Python package with reticulate?") == 1) {
         install.phate()        
       }
     } else if (length(grep("r\\-reticulate", reticulate::py_config()$python)) > 0) {
